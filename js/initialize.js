c0af9caabdbfa2dd6c3e7bc4f7f15b198c7458a8
Christiane Kammerl <christiane@Julius-Kammerls-MacBook-Pro.local>
2014-08-02 12:46:10 -0700

    initializer on page load

---
+++ js/initialize.js
@@ -0,0 +1,12 @@
+// namespace
+Game = {}
+//===================================================================================
+
+// initializer
+
+$(document).ready(function() {
+  var userInputGridWidth = 10; //$('#width').val()
+  var userInputGridHeight = 10; //$('#height').val()
+  var canvas = $('<canvas/>').attr({width: 320, height: 320}).appendTo('body');
+  var controller = new Game.Controller( new Game.View(canvas[0]), new Game.Model(userInputGridWidth, userInputGridHeight), canvas[0]);
+});
