5c7866451f7d80a353d7c65fb641114a0dd7e25e
Christiane Kammerl <christiane@kammerl.com>
2014-09-09 23:16:49 -0700

    refactored

--- js/initialize.js
+++ js/initialize.js
@@ -5,8 +5,8 @@ Game = {}
 // initializer
 
 $(document).ready(function() {
-  var userInputGridWidth = 30 //$('#width').val()
-  var userInputGridHeight = 30 //$('#height').val()
+  var userInputGridWidth = 30
+  var userInputGridHeight = 30
   var canvas = $('#canvas').attr({width: 600, height: 600});
   var controller = new Game.Controller(new Game.View(canvas[0]), new Game.Model(userInputGridWidth, userInputGridHeight), canvas[0]);
 });
